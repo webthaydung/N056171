7423f26c06825a41e758b75e6e087a406baa64d2
Nguyễn Văn Thi <38434830+Kazuto198nd@users.noreply.github.com>
2018-12-21 13:39:38 +0700

    1

--- sources/khach.php
+++ sources/khach.php
@@ -1,8 +1,5 @@
 <?php
-$server_username = "root";
-$server_password = "";
-$server_host = "localhost:88";
-$database = 'de_mo_btl';
- 
-$conn = de_mo_btl($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
-de_mo_btl($conn,"SET NAMES 'UTF8'");
+$conn = new mysqli ('localhost','root', '', 'de_mo_btl') or die("Lỗi kết nối");
+mysqli_query ($conn, 'SET NAMES UTF8');
+// tạo truy vấn csdl
+
--- sources/logout.php
+++ sources/logout.php
@@ -1,11 +1,10 @@
-	<html>
+<html>
     <h1>Đăng Ký</h1>
 	<body>
-
 	<form action="logout.php" method="post">
 		<table>
 			<tr>
-				<td colspan="2">dang ky</td>
+				<td colspan="2">Đăng Ký</td>
 			</tr>	
 			<tr>
 				<td>Username :</td>
@@ -24,11 +23,41 @@
 				<td><input type="text" id="email" name="email"></td>
 			</tr>
 			<tr>
-				<td colspan="2" align="center"><input type="submit" name="btn_submit" value="Dang ky"></td>
+				<td colspan="2" align="center"><input type="submit" name="btn_submit" value="Đăng ký"></td>
 			</tr>
  
 		</table>
- 
 	</form>
+		
+<?php require_once ('khach.php'); ?>
+    <?php
+		if (isset($_POST["btn_submit"])) {
+  			//lấy thông tin từ các form bằng phương thức POST
+  			$username = $_POST["username"];
+  			$password = $_POST["pass"];
+ 			 $hoten = $_POST["hoten"];
+  			$email = $_POST["email"];
+  			//Kiểm tra điều kiện bắt buộc đối với các field không được bỏ trống
+			  if ($username == "" || $password == "" || $hoten == "" || $email == "") {
+				   echo "bạn vui lòng nhập đầy đủ thông tin";
+  			}else{
+  					// Kiểm tra tài khoản đã tồn tại chưa
+  					$sql="select * from users where username='$username'";
+					$kt=mysqli_query($conn, $sql);
+ 
+					if(mysqli_num_row($kt)  > 0){
+						echo "Tài khoản đã tồn tại";
+					}else{
+						//thực hiện việc lưu trữ dữ liệu vào db
+	    				$sql = "INSERT INTO users('"+username+",'"+password+"','"+hoten+"','"+email+"') VALUES ('$username','$password','$hoten','$email')";
+					    // thực thi câu $sql với biến conn lấy từ file connection.php
+   						mysqli_query($conn,$sql);
+				   		echo "chúc mừng bạn đã đăng ký thành công";
+					}
+									    
+					
+			  }
+	}
+	?>
 	</body>
 	</html>
