57aff5b32969ccb752f3863e12d301e03aa29903
Nguyễn Văn Thi <38434830+Kazuto198nd@users.noreply.github.com>
2018-12-21 14:46:48 +0700

    2

--- sources/khach.php
+++ sources/khach.php
@@ -1,5 +1,3 @@
 <?php
 $conn = new mysqli ('localhost','root', '', 'de_mo_btl') or die("Lỗi kết nối");
 mysqli_query ($conn, 'SET NAMES UTF8');
-// tạo truy vấn csdl
-
--- sources/logout.php
+++ sources/logout.php
@@ -1,41 +1,13 @@
 <html>
     <h1>Đăng Ký</h1>
 	<body>
-	<form action="logout.php" method="post">
-		<table>
-			<tr>
-				<td colspan="2">Đăng Ký</td>
-			</tr>	
-			<tr>
-				<td>Username :</td>
-				<td><input type="text" id="username" name="username"></td>
-			</tr>
-			<tr>
-				<td>Password :</td>
-				<td><input type="password" id="pass" name="pass"></td>
-			</tr>
-			<tr>
-				<td>Ho Ten :</td>
-				<td><input type="text" id="name" name="name"></td>
-			</tr>
-			<tr>
-				<td>Email :</td>
-				<td><input type="text" id="email" name="email"></td>
-			</tr>
-			<tr>
-				<td colspan="2" align="center"><input type="submit" name="btn_submit" value="Đăng ký"></td>
-			</tr>
- 
-		</table>
-	</form>
-		
-<?php require_once ('khach.php'); ?>
+	<?php require_once ('khach.php'); ?>
     <?php
 		if (isset($_POST["btn_submit"])) {
   			//lấy thông tin từ các form bằng phương thức POST
   			$username = $_POST["username"];
   			$password = $_POST["pass"];
- 			 $hoten = $_POST["hoten"];
+ 			$hoten = $_POST["hoten"];
   			$email = $_POST["email"];
   			//Kiểm tra điều kiện bắt buộc đối với các field không được bỏ trống
 			  if ($username == "" || $password == "" || $hoten == "" || $email == "") {
@@ -59,5 +31,34 @@
 			  }
 	}
 	?>
+	<form action="logout.php" method="post">
+		<table>
+			<tr>
+				<td colspan="2">Đăng Ký</td>
+			</tr>	
+			<tr>
+				<td>Username :</td>
+				<td><input type="text" id="username" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password :</td>
+				<td><input type="password" id="pass" name="pass"></td>
+			</tr>
+			<tr>
+				<td>Ho Ten :</td>
+				<td><input type="text" id="hoten" name="hoten"></td>
+			</tr>
+			<tr>
+				<td>Email :</td>
+				<td><input type="text" id="email" name="email"></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="center"><input type="submit" name="btn_submit" value="Đăng ký"></td>
+			</tr>
+ 
+		</table>
+	</form>
+		
+
 	</body>
 	</html>
